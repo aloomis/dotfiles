31bf0ad76974cdb7be87737f988fbd552a478988
Andy Loomis <andy.loomis@gmail.com>
2018-04-05 23:20:01 -0700

    Update quad-light to support brightnessctl

--- scripts/quad-light.py
+++ scripts/quad-light.py
@@ -2,19 +2,36 @@
 
 import argparse
 import math
+import os
 import subprocess
 
-def quad_from_linear(x):
+def _quad_from_linear(x):
     return int(round(100 * math.sqrt(x / 100.0)))
 
-def linear_from_quad(x):
+def _linear_from_quad(x):
     return int(round(100 * ((x / 100.0) ** 2)))
 
-def get():
-    return quad_from_linear(float(subprocess.check_output(["light", "-G"])))
+class Backlight(object):
+    def setBrightness(self, x):
+        self._setBrightness(_linear_from_quad(x))
+    def getBrightness(self):
+        return _quad_from_linear(self._getBrightness())
+    def _setBrightness(self, x):
+        raise NotImplementedError("Missing '_setBrightness' implementation.")
+    def _getBrightness(self):
+        raise NotImplementedError("Missing '_getBrightness' implementation.")
 
-def set(x):
-    subprocess.call(["light", "-S", str(linear_from_quad(x))])
+class Light(Backlight):
+    def _setBrightness(self, x):
+        subprocess.call(["light", "-S", str(x)])
+    def _getBrightness(self):
+        return float(subprocess.check_output(["light", "-G"], encoding="utf-8"))
+
+class BrightnessCtl(Backlight):
+    def _setBrightness(self, x):
+        subprocess.call(["brightnessctl", "s", "{0}%%".format(x)], stdout=open(os.devnull, "wb"))
+    def _getBrightness(self):
+        return float(subprocess.check_output(["brightnessctl", "-m"], encoding="utf-8").split(",")[3][:-1])
 
 def main():
     parser = argparse.ArgumentParser(prog="quad-light")
@@ -23,9 +40,11 @@ def main():
     parser.add_argument("-U", type=int, metavar="value", help="Subtract value")
     args = parser.parse_args()
 
-    if args.A: set(get() + args.A)
-    elif args.U: set(get() - args.U)
-    elif args.G: print(get())
+    backlight = BrightnessCtl()
+
+    if args.A: backlight.setBrightness(backlight.getBrightness() + args.A)
+    elif args.U: backlight.setBrightness(backlight.getBrightness() - args.U)
+    elif args.G: print(backlight.getBrightness())
     else: parser.print_help()
 
 if __name__ == "__main__":
